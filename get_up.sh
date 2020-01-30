2a0da56560258bc05958d1ec37ef31776b72944a
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2020-01-30 03:02:37 +0300

    Bash script created

---
+++ get_up.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+FILE=./.env
+if [ -f "$FILE" ]; then
+    echo "production"
+    docker-compose -f docker-compose.yaml up --build -d 
+else
+    echo "development"
+    docker-compose -f docker-compose.yaml up --build -d
+fi
