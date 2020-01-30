c6acba1a52aa1a7dbb2b8703129ab0899e0504fe
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2020-01-30 03:40:16 +0300

    Update get_up.sh

--- get_up.sh
+++ get_up.sh
@@ -2,7 +2,7 @@
 FILE=./.env
 if [ -f "$FILE" ]; then
     echo "production"
-    docker-compose -f docker-compose.yaml up --build -d 
+    docker-compose -f docker-compose-production.yaml up --build -d 
 else
     echo "development"
     docker-compose -f docker-compose.yaml up --build -d
